84af3429d182ffa939d044936e7064b8b614a72c
Orji Dominion <130156592+dodosebn@users.noreply.github.com>
2025-05-01 02:36:43 +0100

    Update stackHolderProfile.tsxprob

--- components/stackHolderProfile.tsx
+++ components/stackHolderProfile.tsx
@@ -18,7 +18,7 @@ const StackHolderProfile = () => {
     >
       <div className="relative"> 
         <StackAnima />
-        <div className="mx-auto flex justify-center relative top-[6.5rem] lg:top-[16.5rem]">
+        <div className="mx-auto flex justify-center relative top-[11rem] lg:top-[17.5rem]">
           <Image
             src={profilePics}
             alt="My Profile"
